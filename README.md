91c71e87dc7f7d5a960a27be3902595398179323
Vorschreibung <vorschreibung@gmail.com>
2024-10-21 18:47:40 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ET:Legacy Release Notes
+e.g.
+
+**Linux**
+```bash
+./convert.sh ./v82.3.0
+```
+
+**Windows**
+```batch
+convert.bat ./v82.3.0
+```
--- convert.bat
+++ convert.bat
@@ -1,8 +1,9 @@
-REM @ECHO OFF
+@ECHO OFF
 
 where /q pixi.exe
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Install pixi first: https://pixi.sh/latest/
+    ECHO Or simply download and rename to 'pixi.exe' and place it next to 'convert.bat'.
     PAUSE
     EXIT /B
 )
